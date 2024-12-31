c742ba8dd521f97c0417cc8edc95949191a4c2b2
jack0sparrow1931 <160811734+jack0sparrow1931@users.noreply.github.com>
2024-12-31 16:41:15 +1100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var add = [1,5,10,20,50]
 var mult = [1,0,0,0,0]
 var speed = [1,0,0,0,0]
 
-setInterval(function{
+setInterval(function() {
   let num1 = add[0] * mult[0]
   let num2 = add[1] * mult[1]
   let num3 = add[2] * mult[2]
@@ -11,4 +11,6 @@ setInterval(function{
   let num5 = add[4] * mult[4]
 
   points += num1 + num2 + num3 + num4 + num5
-}, speed[1] * 1000)
+
+  console.log(points + "|" + num1 + "|" + num2 + "|" + num3 + "|" + num4 + "|" + num5)
+}, speed[0] * 1000)
